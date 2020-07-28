9a515cbd7507b3796ea1df583742e4f27c2bba05
Nathan Kinkade <nkkinkade@gmail.com>
2020-07-28 11:52:39 -0500

    first step done

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 app.use(cors());
 
 // Route Definitions
-
+app.get('/location', locationHandler);
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
@@ -22,6 +22,21 @@ function notFoundHandler(request, response) {
 
 function errorHandler(error, request, response, next) {
   response.status(500).json({ error: true, message: error.message });
+}
+
+function locationHandler(request, response) {
+  const city = 'seattle';
+  const locationData = require('./data/location.json');
+  const location = new Location(city, locationData);
+  response.status(200).send(location);
+}
+
+// Constructors
+function Location(city, locationData) {
+  this.search_query = city;
+  this.formatted_query = locationData[0].display_name;
+  this.latitude = parseFloat(locationData[0].lat);
+  this.longitude = parseFloat(locationData[0].lon);
 }
 
 // App listener
